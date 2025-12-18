7be1d011dde11528357f13b85e47d3ce2e5d036a
TheyloveShaka <shakanathan.z@gmail.com>
2025-12-18 23:17:01 +0300

    Configure GitHub Pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Lulimi-Lingo/',
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'dist'
   }
 })
